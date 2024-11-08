c6c25db9c4653adc3a50ccd1b8aa30e418c20656
JustABoredDude <jovincepro9@gmail.com>
2024-11-08 13:56:02 +0800

    Initial commit

---
+++ About.php
@@ -0,0 +1,94 @@
+ 
+
+<section id="content">
+	<section class="section-padding">
+		<div class="container">
+			<div class="row showcase-section">
+				<div class="col-md-6">
+					<img src="img/dev1.png" alt="showcase image">
+				</div>
+				<div class="col-md-6">
+					<div class="about-text">
+						<h3>Our Team - Diabless</h3>
+						<p>Our team is building a job-seeking website designed to simplify the job search process and connect job seekers with potential employers in an intuitive and efficient way. Each team member brings expertise in user experience, backend development, and design, ensuring that the platform is both functional and engaging. Our goal is to create a site that allows users to easily browse job listings, refine searches based on industry, location, and experience, and apply for positions seamlessly. We are focusing on a clean, professional layout with user-friendly navigation so that job seekers can find relevant opportunities quickly, while employers can manage listings and track applications effortlessly.</p><br>
+ 						<p>To accomplish this, we are employing advanced features like personalized job recommendations, a resume-building tool, and real-time notifications to keep users informed about new job postings. Regular team meetings and feedback loops allow us to refine the platform continuously, ensuring that it meets the needs of our target audience. We’re also prioritizing mobile compatibility to make job searches accessible on any device. Through collaboration and a commitment to innovation, our team aims to create a comprehensive job-seeking platform that fosters meaningful connections and empowers users on their career journeys.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<div class="container">
+						<br>
+						<!-- Our Team starts -->
+				
+						<!-- Heading -->
+						<div class="block-heading-six">
+							<h4 class="bg-color">Our Team</h4>
+						</div>
+						<br>
+						
+						<!-- Our team starts -->
+						
+						<div class="team-six">
+							<div class="row">
+								<div class="col-md-2 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team1.jpg" alt="">
+										<!-- Name -->
+										<h4>Barredo, Khursten</h4>
+										<span class="deg">Creative</span> 
+									</div>
+								</div>
+								<div class="col-md-2 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team2.jpg" alt="">
+										<!-- Name -->
+										<h4>De Guzman, Karlo</h4>
+										<span class="deg">Programmer</span> 
+									</div>
+								</div>
+								<div class="col-md-2 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team3.jpg" alt="">
+										<!-- Name -->
+										<h4>Flameno, Cymon</h4>
+										<span class="deg">CEO</span> 
+									</div>
+								</div>
+								<div class="col-md-2 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team4.jpg" alt="">
+										<!-- Name -->
+										<h4>Lozada, Emar</h4>
+										<span class="deg">Manager</span> 
+									</div>
+								</div>
+								<div class="col-md-2 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team4.jpg" alt="">
+										<!-- Name -->
+										<h4>Salic, Jovince </h4>
+										<span class="deg">Manager</span> 
+									</div>
+								</div>
+							</div>
+						</div>
+					
+						<!-- Our team ends -->
+					  
+						
+					</div>
+									
+				</div>
+	</section> 
+ 

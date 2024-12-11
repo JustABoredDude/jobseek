6f0d817df097e4f1b1c3a97035880c1f6c3797f6
JustABoredDude <jovincepro9@gmail.com>
2024-12-11 20:17:44 +0800

    fixed company's staff side

--- admin/company/controller.php
+++ admin/company/controller.php
@@ -1,94 +1,91 @@
-
 <?php
-require_once ("../../include/initialize.php");
- 	 if (!isset($_SESSION['ADMIN_USERID'])){
-      redirect(web_root."admin/index.php");
-     }
-
+require_once("../../include/initialize.php");
+if (!isset($_SESSION['ADMIN_USERID'])) {
+    redirect(web_root . "admin/index.php");
+}
 
 $action = (isset($_GET['action']) && $_GET['action'] != '') ? $_GET['action'] : '';
 
 switch ($action) {
-	case 'add' :
-	doInsert();
-	break;
-	
-	case 'edit' :
-	doEdit();
-	break;
-	
-	case 'delete' :
-	doDelete();
-	break;
+    case 'add':
+        if (isAdmin()) {
+            doInsert();
+        } else {
+            unauthorizedRedirect();
+        }
+        break;
 
- 
-	}
-   
-	function doInsert(){
-		if(isset($_POST['save'])){
+    case 'edit':
+        if (isAdmin()) {
+            doEdit();
+        } else {
+            unauthorizedRedirect();
+        }
+        break;
 
- // `COMPANYNAME`, `COMPANYADDRESS`, `COMPANYCONTACTNO`
-		if ( $_POST['COMPANYNAME'] == "" || $_POST['COMPANYADDRESS'] == "" || $_POST['COMPANYCONTACTNO'] == "" ) {
-			$messageStats = false;
-			message("All field is required!","error");
-			redirect('index.php?view=add');
-		}else{	
-			$company = New Company();
-			$company->COMPANYNAME		= $_POST['COMPANYNAME'];
-			$company->COMPANYADDRESS	= $_POST['COMPANYADDRESS'];
-			$company->COMPANYCONTACTNO	= $_POST['COMPANYCONTACTNO'];
-			$company->create();
+    case 'delete':
+        if (isAdmin()) {
+            doDelete();
+        } else {
+            unauthorizedRedirect();
+        }
+        break;
+}
 
-			message("New company created successfully!", "success");
-			redirect("index.php");
-			
-		}
-		}
+function isAdmin()
+{
+    return isset($_SESSION['ADMIN_ROLE']) && $_SESSION['ADMIN_ROLE'] == 'Administrator';
+}
 
-	}
+function unauthorizedRedirect()
+{
+    message("You do not have permission to perform this action.", "error");
+    redirect("index.php");
+}
 
-	function doEdit(){
-		if(isset($_POST['save'])){
+function doInsert()
+{
+    if (isset($_POST['save'])) {
+        if ($_POST['COMPANYNAME'] == "" || $_POST['COMPANYADDRESS'] == "" || $_POST['COMPANYCONTACTNO'] == "") {
+            message("All fields are required!", "error");
+            redirect('index.php?view=add');
+        } else {
+            $company = new Company();
+            $company->COMPANYNAME = $_POST['COMPANYNAME'];
+            $company->COMPANYADDRESS = $_POST['COMPANYADDRESS'];
+            $company->COMPANYCONTACTNO = $_POST['COMPANYCONTACTNO'];
+            $company->create();
 
-			$company = New Company();
-			$company->COMPANYNAME		= $_POST['COMPANYNAME'];
-			$company->COMPANYADDRESS	= $_POST['COMPANYADDRESS'];
-			$company->COMPANYCONTACTNO	= $_POST['COMPANYCONTACTNO'];
-			$company->update($_POST['COMPANYID']);
+            message("New company created successfully!", "success");
+            redirect("index.php");
+        }
+    }
+}
 
-			message("Company has been updated!", "success");
-			redirect("index.php");
-		}
+function doEdit()
+{
+    if (isset($_POST['save'])) {
+        $company = new Company();
+        $company->COMPANYNAME = $_POST['COMPANYNAME'];
+        $company->COMPANYADDRESS = $_POST['COMPANYADDRESS'];
+        $company->COMPANYCONTACTNO = $_POST['COMPANYCONTACTNO'];
+        $company->update($_POST['COMPANYID']);
 
-	}
+        message("Company has been updated!", "success");
+        redirect("index.php");
+    }
+}
 
+function doDelete()
+{
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
 
-	function doDelete(){
-		// if (isset($_POST['selector'])==''){
-		// message("Select a records first before you delete!","error");
-		// redirect('index.php');
-		// }else{
+        $company = new Company();
+        $company->delete($id);
 
-			$id = $_GET['id'];
-
-			$company = New Company();
-			$company->delete($id);
-
-			message("Company has been Deleted!","info");
-			redirect('index.php');
-
-		// $id = $_POST['selector'];
-		// $key = count($id);
-
-		// for($i=0;$i<$key;$i++){
-
-		// 	$category = New Category();
-		// 	$category->delete($id[$i]);
-
-		// 	message("Category already Deleted!","info");
-		// 	redirect('index.php');
-		// }
-		// }
-		
-	}
+        message("Company has been deleted!", "info");
+        redirect("index.php");
+    }
+}
 ?>

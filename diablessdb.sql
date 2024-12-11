acd529982e538a33b505d202bcdbe1eedce8936b
JustABoredDude <jovincepro9@gmail.com>
2024-12-11 16:21:18 +0800

    added sql file

---
+++ diablessdb.sql
@@ -0,0 +1,462 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 11, 2024 at 09:19 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `diablessdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblapplicants`
+--
+
+CREATE TABLE `tblapplicants` (
+  `APPLICANTID` int(11) NOT NULL,
+  `FNAME` varchar(90) NOT NULL,
+  `LNAME` varchar(90) NOT NULL,
+  `MNAME` varchar(90) NOT NULL,
+  `ADDRESS` varchar(255) NOT NULL,
+  `SEX` varchar(11) NOT NULL,
+  `CIVILSTATUS` varchar(30) NOT NULL,
+  `BIRTHDATE` date NOT NULL,
+  `BIRTHPLACE` varchar(255) NOT NULL,
+  `AGE` int(2) NOT NULL,
+  `USERNAME` varchar(90) NOT NULL,
+  `PASS` varchar(90) NOT NULL,
+  `EMAILADDRESS` varchar(90) NOT NULL,
+  `CONTACTNO` varchar(90) NOT NULL,
+  `DEGREE` text NOT NULL,
+  `APPLICANTPHOTO` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblapplicants`
+--
+
+INSERT INTO `tblapplicants` (`APPLICANTID`, `FNAME`, `LNAME`, `MNAME`, `ADDRESS`, `SEX`, `CIVILSTATUS`, `BIRTHDATE`, `BIRTHPLACE`, `AGE`, `USERNAME`, `PASS`, `EMAILADDRESS`, `CONTACTNO`, `DEGREE`, `APPLICANTPHOTO`) VALUES
+(2024023, 'jdsai', 'dajd', 'jdai', 'diaj', 'Female', 'Married', '2000-01-31', 'a', 24, 'lol', 'lol', 'jdia@gmail.com', '09999', 'dlasp', 'photos/IMG_20230715_075302.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblattachmentfile`
+--
+
+CREATE TABLE `tblattachmentfile` (
+  `ID` int(11) NOT NULL,
+  `FILEID` varchar(30) DEFAULT NULL,
+  `JOBID` int(11) NOT NULL,
+  `FILE_NAME` varchar(90) NOT NULL,
+  `FILE_LOCATION` varchar(255) NOT NULL,
+  `USERATTACHMENTID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblattachmentfile`
+--
+
+INSERT INTO `tblattachmentfile` (`ID`, `FILEID`, `JOBID`, `FILE_NAME`, `FILE_LOCATION`, `USERATTACHMENTID`) VALUES
+(2, '2147483647', 2, 'Resume', 'photos/27052018124027PLATENO FE95483.docx', 2018013),
+(3, '20246912532', 3, 'Resume', 'photos/14112024091631narrative-report-template.docx', 2024022),
+(4, '20246912534', 3, 'Resume', 'photos/01122024065909DesignDevelopmentandUsabilityEvaluationofanOnlineWeb-basedLostand.pdf', 2024023);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblautonumbers`
+--
+
+CREATE TABLE `tblautonumbers` (
+  `AUTOID` int(11) NOT NULL,
+  `AUTOSTART` varchar(30) NOT NULL,
+  `AUTOEND` int(11) NOT NULL,
+  `AUTOINC` int(11) NOT NULL,
+  `AUTOKEY` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblautonumbers`
+--
+
+INSERT INTO `tblautonumbers` (`AUTOID`, `AUTOSTART`, `AUTOEND`, `AUTOINC`, `AUTOKEY`) VALUES
+(1, '02983', 12, 1, 'userid'),
+(2, '000', 79, 1, 'employeeid'),
+(3, '0', 24, 1, 'APPLICANT'),
+(4, '69125', 35, 1, 'FILEID');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcategory`
+--
+
+CREATE TABLE `tblcategory` (
+  `CATEGORYID` int(11) NOT NULL,
+  `CATEGORY` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblcategory`
+--
+
+INSERT INTO `tblcategory` (`CATEGORYID`, `CATEGORY`) VALUES
+(10, 'Technology'),
+(11, 'Managerial'),
+(12, 'Engineer'),
+(13, 'IT'),
+(14, 'Civil Engineer'),
+(15, 'HR'),
+(23, 'Sales'),
+(24, 'Banking'),
+(25, 'Finance'),
+(26, 'BPO'),
+(27, 'Degital Marketing'),
+(28, 'Shipping');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcompany`
+--
+
+CREATE TABLE `tblcompany` (
+  `COMPANYID` int(11) NOT NULL,
+  `COMPANYNAME` varchar(90) NOT NULL,
+  `COMPANYADDRESS` varchar(90) NOT NULL,
+  `COMPANYCONTACTNO` varchar(30) NOT NULL,
+  `COMPANYSTATUS` varchar(90) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblcompany`
+--
+
+INSERT INTO `tblcompany` (`COMPANYID`, `COMPANYNAME`, `COMPANYADDRESS`, `COMPANYCONTACTNO`, `COMPANYSTATUS`) VALUES
+(2, 'URC', 'Bry Camugao', '023654', ''),
+(3, 'Kupal', 'Mabinay\'s', '4328811', ''),
+(4, 'Quest', 'Kabankalan City', '23165', ''),
+(6, 'Palacios Company', 'Cubao Ibabaw', '0625656899', ''),
+(7, 'IT Company', 'Cubao Ilalim', '04564123', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblemployees`
+--
+
+CREATE TABLE `tblemployees` (
+  `INCID` int(11) NOT NULL,
+  `EMPLOYEEID` varchar(30) NOT NULL,
+  `FNAME` varchar(50) NOT NULL,
+  `LNAME` varchar(50) NOT NULL,
+  `MNAME` varchar(50) NOT NULL,
+  `ADDRESS` varchar(90) NOT NULL,
+  `BIRTHDATE` date NOT NULL,
+  `BIRTHPLACE` varchar(90) NOT NULL,
+  `AGE` int(11) NOT NULL,
+  `SEX` varchar(30) NOT NULL,
+  `CIVILSTATUS` varchar(30) NOT NULL,
+  `TELNO` varchar(40) NOT NULL,
+  `EMP_EMAILADDRESS` varchar(90) NOT NULL,
+  `CELLNO` varchar(30) NOT NULL,
+  `POSITION` varchar(50) NOT NULL,
+  `EMPPHOTO` varchar(255) NOT NULL,
+  `EMPUSERNAME` varchar(90) NOT NULL,
+  `EMPPASSWORD` varchar(125) NOT NULL,
+  `DATEHIRED` date NOT NULL,
+  `COMPANYID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblemployees`
+--
+
+INSERT INTO `tblemployees` (`INCID`, `EMPLOYEEID`, `FNAME`, `LNAME`, `MNAME`, `ADDRESS`, `BIRTHDATE`, `BIRTHPLACE`, `AGE`, `SEX`, `CIVILSTATUS`, `TELNO`, `EMP_EMAILADDRESS`, `CELLNO`, `POSITION`, `EMPPHOTO`, `EMPUSERNAME`, `EMPPASSWORD`, `DATEHIRED`, `COMPANYID`) VALUES
+(76, '2018001', 'Chambe', 'Narciso', 'Captain', 'mabinay', '1992-01-23', 'Mabinay', 26, 'Male', 'Married', '032656', 'chambe@yahoo.com', '', 'Fuel Tender', '', '2018001', 'f3593fd40c55c33d1788309d4137e82f5eab0dea', '2018-05-23', 2),
+(77, '101', 'dsad', 'sad', 'mkmk', 'mdskd', '1990-06-12', 'mvkcmv', 34, 'Female', 'Widow', '09991', 'shiro@gmail.com', '', 'Staff', '', '101', '101', '2024-10-02', 3),
+(78, '6969', 'djsaid', 'sajdao', 'neok', 'ksadsa', '2000-02-16', 'cubao', 24, 'Female', 'Single', '09009090', 'jdsaid@gmail.com', '', '69', '', '6969', '6969', '2009-12-01', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblfeedback`
+--
+
+CREATE TABLE `tblfeedback` (
+  `FEEDBACKID` int(11) NOT NULL,
+  `APPLICANTID` int(11) NOT NULL,
+  `REGISTRATIONID` int(11) NOT NULL,
+  `FEEDBACK` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblfeedback`
+--
+
+INSERT INTO `tblfeedback` (`FEEDBACKID`, `APPLICANTID`, `REGISTRATIONID`, `FEEDBACK`) VALUES
+(2, 2024022, 3, 'Pending');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbljob`
+--
+
+CREATE TABLE `tbljob` (
+  `JOBID` int(11) NOT NULL,
+  `COMPANYID` int(11) NOT NULL,
+  `CATEGORY` varchar(250) NOT NULL,
+  `OCCUPATIONTITLE` varchar(90) NOT NULL,
+  `REQ_NO_EMPLOYEES` int(11) NOT NULL,
+  `SALARIES` double NOT NULL,
+  `DURATION_EMPLOYEMENT` varchar(90) NOT NULL,
+  `QUALIFICATION_WORKEXPERIENCE` text NOT NULL,
+  `JOBDESCRIPTION` text NOT NULL,
+  `PREFEREDSEX` varchar(30) NOT NULL,
+  `SECTOR_VACANCY` text NOT NULL,
+  `JOBSTATUS` varchar(90) NOT NULL,
+  `DATEPOSTED` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tbljob`
+--
+
+INSERT INTO `tbljob` (`JOBID`, `COMPANYID`, `CATEGORY`, `OCCUPATIONTITLE`, `REQ_NO_EMPLOYEES`, `SALARIES`, `DURATION_EMPLOYEMENT`, `QUALIFICATION_WORKEXPERIENCE`, `JOBDESCRIPTION`, `PREFEREDSEX`, `SECTOR_VACANCY`, `JOBSTATUS`, `DATEPOSTED`) VALUES
+(1, 2, 'Technology', 'ISD', 6, 15000, 'jan 30', 'Two year Experience', 'We are looking for bachelor of science in information technology.\r\nasdasdasd', 'Male/Female', 'yes', '', '2018-05-20 00:00:00'),
+(2, 2, 'Technology', 'Accounting', 1, 15000, 'may 20', 'Two years Experience', 'We are looking for bachelor of science in Acountancy', 'Female', 'yes', '', '2018-05-20 02:33:00'),
+(3, 3, 'Civil Engineer', 'cope', 1, 1000000000000, '1month', 'dab', 'dab', 'Male/Female', 'e', '', '2024-11-13 00:50:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbljobregistration`
+--
+
+CREATE TABLE `tbljobregistration` (
+  `REGISTRATIONID` int(11) NOT NULL,
+  `COMPANYID` int(11) NOT NULL,
+  `JOBID` int(11) NOT NULL,
+  `APPLICANTID` int(11) NOT NULL,
+  `APPLICANT` varchar(90) NOT NULL,
+  `REGISTRATIONDATE` date NOT NULL,
+  `REMARKS` varchar(255) NOT NULL DEFAULT 'Pending',
+  `FILEID` varchar(30) DEFAULT NULL,
+  `PENDINGAPPLICATION` tinyint(1) NOT NULL DEFAULT 1,
+  `HVIEW` tinyint(1) NOT NULL DEFAULT 1,
+  `DATETIMEAPPROVED` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tbljobregistration`
+--
+
+INSERT INTO `tbljobregistration` (`REGISTRATIONID`, `COMPANYID`, `JOBID`, `APPLICANTID`, `APPLICANT`, `REGISTRATIONDATE`, `REMARKS`, `FILEID`, `PENDINGAPPLICATION`, `HVIEW`, `DATETIMEAPPROVED`) VALUES
+(1, 2, 2, 2018013, 'Kim Domingo', '2018-05-27', 'Ive seen your work and its really interesting', '2147483647', 0, 1, '2018-05-26 16:13:01'),
+(2, 2, 2, 2018015, 'Janry Tan', '2018-05-26', 'aasd', '2147483647', 0, 0, '2018-05-28 14:14:45'),
+(3, 3, 3, 2024022, 'fdqf mska', '2024-11-14', 'Pending', '20246912532', 0, 0, '2024-11-14 16:30:17'),
+(4, 3, 3, 2024023, 'jdsai dajd', '2024-12-01', 'Pending', '20246912534', 1, 1, '2024-12-01 06:59:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbltodo`
+--
+
+CREATE TABLE `tbltodo` (
+  `id` int(11) NOT NULL,
+  `task_text` varchar(255) NOT NULL,
+  `completed` tinyint(1) DEFAULT 0,
+  `label_class` varchar(50) DEFAULT NULL,
+  `label_text` varchar(50) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbltodo`
+--
+
+INSERT INTO `tbltodo` (`id`, `task_text`, `completed`, `label_class`, `label_text`, `created_at`) VALUES
+(1, 'ndaj', 1, 'njndajnd', 'ndasjd', '2024-11-14 02:55:18');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblusers`
+--
+
+CREATE TABLE `tblusers` (
+  `USERID` varchar(30) NOT NULL,
+  `FULLNAME` varchar(40) NOT NULL,
+  `USERNAME` varchar(90) NOT NULL,
+  `PASS` varchar(90) NOT NULL,
+  `ROLE` varchar(30) NOT NULL,
+  `PICLOCATION` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblusers`
+--
+
+INSERT INTO `tblusers` (`USERID`, `FULLNAME`, `USERNAME`, `PASS`, `ROLE`, `PICLOCATION`) VALUES
+('0298310', 'staff', 'staff', 'staff', 'Staff', ''),
+('0298311', 'uluru', 'uluru', 'uluru', 'Staff', ''),
+('1', 'dab', 'dab', 'dab', 'Administrator', 'photos/Sleep.jpg'),
+('6969', 'djsaid sajdao', 'sajdao', '6969', 'Employee', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tblapplicants`
+--
+ALTER TABLE `tblapplicants`
+  ADD PRIMARY KEY (`APPLICANTID`);
+
+--
+-- Indexes for table `tblattachmentfile`
+--
+ALTER TABLE `tblattachmentfile`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblautonumbers`
+--
+ALTER TABLE `tblautonumbers`
+  ADD PRIMARY KEY (`AUTOID`);
+
+--
+-- Indexes for table `tblcategory`
+--
+ALTER TABLE `tblcategory`
+  ADD PRIMARY KEY (`CATEGORYID`);
+
+--
+-- Indexes for table `tblcompany`
+--
+ALTER TABLE `tblcompany`
+  ADD PRIMARY KEY (`COMPANYID`);
+
+--
+-- Indexes for table `tblemployees`
+--
+ALTER TABLE `tblemployees`
+  ADD PRIMARY KEY (`INCID`),
+  ADD UNIQUE KEY `EMPLOYEEID` (`EMPLOYEEID`);
+
+--
+-- Indexes for table `tblfeedback`
+--
+ALTER TABLE `tblfeedback`
+  ADD PRIMARY KEY (`FEEDBACKID`);
+
+--
+-- Indexes for table `tbljob`
+--
+ALTER TABLE `tbljob`
+  ADD PRIMARY KEY (`JOBID`);
+
+--
+-- Indexes for table `tbljobregistration`
+--
+ALTER TABLE `tbljobregistration`
+  ADD PRIMARY KEY (`REGISTRATIONID`);
+
+--
+-- Indexes for table `tbltodo`
+--
+ALTER TABLE `tbltodo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblusers`
+--
+ALTER TABLE `tblusers`
+  ADD PRIMARY KEY (`USERID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tblapplicants`
+--
+ALTER TABLE `tblapplicants`
+  MODIFY `APPLICANTID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2024024;
+
+--
+-- AUTO_INCREMENT for table `tblattachmentfile`
+--
+ALTER TABLE `tblattachmentfile`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tblautonumbers`
+--
+ALTER TABLE `tblautonumbers`
+  MODIFY `AUTOID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tblcategory`
+--
+ALTER TABLE `tblcategory`
+  MODIFY `CATEGORYID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- AUTO_INCREMENT for table `tblcompany`
+--
+ALTER TABLE `tblcompany`
+  MODIFY `COMPANYID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `tblemployees`
+--
+ALTER TABLE `tblemployees`
+  MODIFY `INCID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
+
+--
+-- AUTO_INCREMENT for table `tblfeedback`
+--
+ALTER TABLE `tblfeedback`
+  MODIFY `FEEDBACKID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tbljob`
+--
+ALTER TABLE `tbljob`
+  MODIFY `JOBID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tbljobregistration`
+--
+ALTER TABLE `tbljobregistration`
+  MODIFY `REGISTRATIONID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tbltodo`
+--
+ALTER TABLE `tbltodo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
